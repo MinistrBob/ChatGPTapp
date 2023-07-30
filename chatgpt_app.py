ae75d5f276981f05b6be2f2453ab1a19a02ae7df
Бобровский Дмитрий <ministrbob777@gmail.com>
2023-07-30 14:24:17 +0300

    Цикл по текстовому файлу БЕЗ фильтрацией дублей.

--- chatgpt_app.py
+++ chatgpt_app.py
@@ -14,19 +14,28 @@ openai.api_key = settings.openai_api_key
 openai.organization = settings.openai_organization
 
 
-def query2(prompt):
-    prompt = "составь описание автозапчасти примерно на 1000 символов без наименования:" + "\n" + prompt
+def query2(prompt, model="gpt-3.5-turbo"):
+    """
+    Запрос к CG
+    :param prompt: Текст запроса (можно много строк)
+    :param model: Модель CG см. файл list_of_models.txt
+    :return: Ответ на запрос.
+    """
     # try:
     #     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k-0613",
     #                                                    messages=[{"role": "user", "content": prompt}])
     # except:
     #     raise
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k-0613",
+    chat_completion = openai.ChatCompletion.create(model=model,
                                                    messages=[{"role": "user", "content": prompt}])
     return chat_completion.choices[0].message.content.rstrip('\n')
 
 
 def telegram_notification(message):
+    """
+    Оповещения в Телеграм. Настройки берутся из SETTINGS.py.
+    :param message: Сообщение отправляемое в Телеграм.
+    """
     # headers = {
     #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)     Chrome/37.0.2049.0 Safari/537.36'
     # }
@@ -48,19 +57,24 @@ def telegram_notification(message):
     # print(f"resp={resp}")
 
 
-if __name__ == '__main__':
+def mass_query_01():
+    """
+    Цикл по текстовому файлу с фильтрацией дублей. Для каждого дубля просто берётся готовое описание, сформированное CG. Т.е. для всех дублей получаются одни и те же описания.
+    :return:
+    """
     start = time.time()  # Текущее время
+    prefix = "составь описание автозапчасти примерно на 1000 символов без наименования:\n"
     # Обработка списка товаров из текстового файла
     workbook = xlsxwriter.Workbook(settings.excel_file)
     try:
         worksheet = workbook.add_worksheet('Список')
         with open(settings.journal_text_file, "w") as output:
             with open(settings.text_file, "r", encoding="utf-8") as f:
-                i = 1  # Индекс текущей строки
-                u = 1  # Индекс уникальных строки
+                i = 0  # Индекс текущей строки
+                u = 0  # Индекс уникальных строки
                 first_line = f.readline().strip('\n')  # Первая строка
                 previous_line = first_line
-                result = query2(first_line)
+                result = query2(prefix + first_line)
                 print(f"{str(i).zfill(4)} | {time.time() - start} сек. | {first_line}")
                 worksheet.write(i, 0, first_line)
                 worksheet.write(i, 1, result)
@@ -71,7 +85,7 @@ if __name__ == '__main__':
                     if line == previous_line:
                         u += 1
                     else:
-                        result = query2(line)
+                        result = query2(prefix + line)
                         previous_line = line
                     worksheet.write(i, 0, line)
                     worksheet.write(i, 1, result)
@@ -99,3 +113,65 @@ if __name__ == '__main__':
     message_text = f"Время работы программы: {end} сек. | Всего строк  {i} | Всего уникальных строк {u}"
     print(message_text)
     telegram_notification(f"✅ {message_text}")
+
+
+def mass_query_02():
+    """
+    Цикл по текстовому файлу БЕЗ фильтрацией дублей. Т.е. для каждой строки формируется своё описание.
+    :return:
+    """
+    start = time.time()  # Текущее время
+    prefix = "составь описание автозапчасти примерно на 1000 символов без наименования:\n"
+    # Обработка списка товаров из текстового файла
+    workbook = xlsxwriter.Workbook(settings.excel_file)
+    try:
+        worksheet = workbook.add_worksheet('Список')
+        with open(settings.journal_text_file, "w") as output:
+            with open(settings.text_file, "r", encoding="utf-8") as f:
+                i = 0  # Индекс текущей строки
+                for line in f:
+                    line = line.strip('\n')
+                    result = query2(prefix + line)
+                    worksheet.write(i, 0, line)
+                    worksheet.write(i, 1, result)
+                    output.write(f"{line}\t{result}\n")
+                    i += 1
+                    print(f"{str(i).zfill(4)} | {time.time() - start} сек. | {line}")
+            workbook.close()
+    except Exception as e:
+        workbook.close()
+        f.close()
+        output.close()
+        err_text = f"Error: {e}"
+        # err_line = f"{str(i).zfill(4)} | {time.time() - start} сек. | {line}"
+        err_stack = traceback.format_exc()
+        print(err_text)
+        # print(err_line)
+        print(err_stack)
+        # print(f"=111==========================================")
+        telegram_notification(f"❌ {err_text}")
+        # telegram_notification(err_line)
+        # print(f"=222==========================================")
+        # telegram_notification(f"<code>{err_stack}</code>")
+
+    end = time.time() - start  # собственно время работы программы
+    message_text = f"Время работы программы: {end} сек. | Всего строк  {i}"
+    print(message_text)
+    telegram_notification(f"✅ {message_text}")
+
+
+def test_query():
+    """
+    Тестирование запроса.
+    """
+    print(query2("""Я продаю автозапчасти на сайте. Составь разное описание для каждой автозапчасти длиной от 1000 до 1500 символов: 
+    Амортизатор задний (стойка) CX-5 (KE) 2012-2016
+    Амортизатор задний (стойка) CX-5 (KE) 2012-2018
+    Амортизатор задний (стойка) CX-5 (KE) 2012-2016
+    Амортизатор задний (стойка) CX-5 (KE) 2012-2016""", model="gpt-3.5-turbo"))
+
+
+if __name__ == '__main__':
+    # test_query()
+    # mass_query_01()
+    mass_query_02()
